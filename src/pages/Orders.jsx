a766a9bda99276edb59e40007c92fb482081321a
Frainer Encarnación <fraineralex2001@gmail.com>
2023-07-11 00:16:27 -0400

    [WIP] working in the project layout

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Orders = () => (
+  <div className="mt-24">
+  </div>
+);
+export default Orders;
