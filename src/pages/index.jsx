97da45f7dc37fb2ea052b067b99995bf15d02b52
Frainer Encarnación <fraineralex2001@gmail.com>
2023-07-22 23:37:17 -0400

    [WIP] working in the Employees component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,6 @@ export { default as Expenses } from './Expenses';
 export { default as Receipts } from './Receipts';
 export { default as Balances } from './Balances';
 export { default as Collections } from './Collections';
-export { default as Employees } from './Employees';
+export { default as Employees } from './Employees.tsx';
 export { default as Customers } from './Customers';
 export { default as Calendar } from './Calendar';
